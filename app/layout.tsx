5c7f6b295bd35dcd39af785c67ae402bda99cc51
Muhammad Muneeb <101018722+MalikMuneeb20@users.noreply.github.com>
2023-11-24 02:55:28 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
         <div>
           <TopNav />
         </div>
-        <div className="p-2 flex-row h-full hidden md:flex">
-          <div className="p-2 ">
+        <div className="pl-6 pr-6  pt-4 pb-4 flex-row h-full hidden md:flex">
+          <div className="pr-2 ">
             <SideNav />
           </div>
           <div style={{ border: '1px solid black' }} className="w-full pl-5">
