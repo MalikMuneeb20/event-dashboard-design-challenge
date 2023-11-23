73e020e470c90a4310417b8981544b82fa89fa40
Muhammad Muneeb <101018722+MalikMuneeb20@users.noreply.github.com>
2023-11-24 02:58:35 +0500

    Update page.tsx

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FavouritesDashboard = () => {
-  return <div>FavouritesDashboard</div>;
+  return <div>Favourites</div>;
 };
 
 export default FavouritesDashboard;
